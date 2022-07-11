a66755bf358cd582c7987f5db1739422fb5476f3
Chris Stewart <Chris.Stewart@atco.com>
2022-07-10 18:48:04 -0600

    Day 5: Parsing

--- 05/main.py
+++ 05/main.py
@@ -1,31 +1,75 @@
 import easygui
 import time
 
-AOCDAY = "04"
+from numpy import true_divide
+
+AOCDAY = "05"
 
 def readFile(fileName):
     with open(fileName, "r") as file:
-        return file.readlines()
+        lines = file.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].rstrip()
+    return lines
 
-def md5(s):
-    import hashlib
-    return hashlib.md5(s.encode('utf-8')).hexdigest()
+def part1NaughtyCheck(line):
+    naughtyCombo = ["ab", "cd", "pq", "xy"]
+    vowelCount = 0
+    for i in range(len(line)):
+        if line[i] in "aeiou":
+            vowelCount += 1
+    doubleLetter = False
+    for i in range(len(line)-1):
+        if line[i] == line[i+1]:
+            doubleLetter = True
+    naughtyComboFound = False
+    for combo in naughtyCombo:
+        if combo in line:
+            naughtyComboFound = True
+    return (vowelCount < 3 or (not doubleLetter) or naughtyComboFound)    
+
+def doubleDuoCheck(line):
+    duoDict = {}
+    doubleDuo = False
+    for i in range(len(line)-1):
+        duo = line[i] + line[i+1]
+        if (duo) in duoDict:
+            duoDict[duo] += 1
+            if line[i-1] == line[i] and line[i-1] == line[i+1]:
+                duoDict[duo] -= 1
+        else:
+            duoDict[duo] = 1
+    for key in duoDict:
+        if duoDict[key] > 1:
+            doubleDuo = True
+    return doubleDuo
+
+def part2NaughtyCheck(line):
+    doubleDuo = doubleDuoCheck(line)
+    quad = False
+    for i in range(len(line)-3):
+        if line[i] == line[i+1] and line[i] == line[i+2] and line[i] == line[i+3]:
+            quad = True
+    spacedDuo = False
+    for i in range(len(line)-2):
+        if line[i] == line[i+2]:
+            spacedDuo = True
+
+    return (not ((doubleDuo or quad) and spacedDuo))    
 
 def part1(lines):
-    calcedHash = "fffffffffffff"
-    num = -1
-    while calcedHash [:5] != "00000":
-        num += 1
-        calcedHash = md5(lines[0] + str(num))
-    return(f"The number is {num}.")
-
+    niceCount = 0
+    for line in lines:
+        if not part1NaughtyCheck(line):
+            niceCount += 1
+    return(f"There are {niceCount} nice strings.")
+        
 def part2(lines):
-    calcedHash = "fffffffffffff"
-    num = -1
-    while calcedHash [:6] != "000000":
-        num += 1
-        calcedHash = md5(lines[0] + str(num))
-    return(f"The number is {num}.")
+    niceCount = 0
+    for line in lines:
+        if not part2NaughtyCheck(line):
+            niceCount += 1
+    return(f"There are {niceCount} nice strings.")
 
 def main ():
     fileName = easygui.fileopenbox(default=f"./"+AOCDAY+"/"+"*.txt")
